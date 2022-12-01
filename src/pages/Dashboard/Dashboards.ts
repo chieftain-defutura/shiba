cada92b89141a56451ccc4c3a71e90ec90c67c4c
suganya05 <go2suganya@gmail.com>
2022-12-01 19:23:57 +0530

    pull

---
+++ src/pages/Dashboard/Dashboards.ts
@@ -0,0 +1,44 @@
+import NftImg from "../../assets/img/nftMintImg.png";
+import ChatImg from "../../assets/img/chatImg.png";
+import DashiboardImg from "../../assets/img/dashiboardImg.png";
+import HelpImg from "../../assets/img/helpFAQImg.png";
+
+export const DemoDashboard = [
+  {
+    title: "NFT MINT",
+    description:
+      "Lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+    image: {
+      src: NftImg,
+      alt: "nft",
+    },
+    link: "mint-nft",
+  },
+  {
+    title: "NFT MINT",
+    description:
+      "Lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    image: {
+      src: ChatImg,
+      alt: "nft",
+    },
+  },
+  {
+    title: "NFT MINT",
+    description:
+      "Lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    image: {
+      src: DashiboardImg,
+      alt: "nft",
+    },
+  },
+  {
+    title: "NFT MINT",
+    description:
+      "Lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    image: {
+      src: HelpImg,
+      alt: "nft",
+    },
+  },
+];
