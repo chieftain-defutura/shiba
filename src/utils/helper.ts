c4f0e10d49777ab86d518681cbda2ba5f2670082
subaa P <subaap86@gmail.com>
2023-02-03 10:21:56 +0530

    chore: pull origin main

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,6 +1,13 @@
 const getApp = () => {
   let host = window.location.host
-  const arr = host.split('.').slice(0, host.includes('localhost') ? -1 : -2)
+  let arr: string[] = []
+  console.log(host.split('.').slice(0, 2))
+  if (process.env.NODE_ENV === 'development') {
+    arr = host.split('.').slice(0, host.includes('localhost') ? -1 : -2)
+  } else {
+    arr = host.split('.').slice(0, 2)
+  }
+
   if (arr.length > 0) {
     host =
       window.location.protocol +
